96d6d6161c72cc39804cc8dae470a59a301c2f7f
Zach Wilson <zach.wilson@wizard.dev>
2023-06-13 21:18:14 -0700

    Switch to including script from file.
    The deployment process seems to collapse the script section into a single line.
    Since the script starts with a comment, that causes the entire script to be
    commented out and not executed.

---
+++ assets/js/avatar.js
@@ -0,0 +1,69 @@
+// Available avatars
+const avatarImgFolder = "/assets/img";
+const orangeAvatarPath = `${avatarImgFolder}/logo-orange.png`;
+const orangeBorderColor = "#FF8827";
+const purpleAvatarPath = `${avatarImgFolder}/logo-purple.png`;
+const purpleBorderColor = "#33006F";
+const goldAvatarPath = `${avatarImgFolder}/logo-gold.png`;
+const goldBorderColor = "#917b4c";
+
+// Bind to the mode switcher button and toggle the avatar image
+const modeSwitcher = document.getElementsByClassName("mode-toggle")[0];
+const lightThemeAvatarPath = goldAvatarPath;
+const lightThemeBorderColor = goldBorderColor;
+const darkThemeAvatarPath = purpleAvatarPath;
+const darkThemeBorderColor = purpleBorderColor;
+
+// Chirpy will add `data-mode="light"` to the html element if in light
+//   theme and will not have the attribute at all if in dark theme
+var isCurrentlyLightTheme =
+	document.documentElement.hasAttribute("data-mode");
+
+// Helper methods
+const getAvatarElement = () => {
+	return document.querySelector("#avatar");
+};
+const getAvatarImage = () => {
+	return document.querySelector("#avatar img");
+};
+const makeSetThemeFunc = (imageSrc, borderColor, isLightTheme) => {
+	const avatar = getAvatarElement();
+	const avatarImage = getAvatarImage();
+
+	return () => {
+		avatar.style.setProperty(
+			"--avatar-border-color",
+			borderColor
+		);
+		avatarImage.src = imageSrc;
+		isCurrentlyLightTheme = isLightTheme;
+	};
+};
+const setDarkTheme = makeSetThemeFunc(
+	darkThemeAvatarPath,
+	darkThemeBorderColor,
+	false
+);
+const setLightTheme = makeSetThemeFunc(
+	lightThemeAvatarPath,
+	lightThemeBorderColor,
+	true
+);
+
+// Make sure that the avatar gets set to the current theme's avatar
+// This is needed because the _config.yml defaults the avatar to the dark
+//   theme avatar, but the user could already be in the light theme
+if (isCurrentlyLightTheme) {
+	setLightTheme();
+} else {
+	setDarkTheme();
+}
+
+// Bind to the mode switcher to update the avatar
+modeSwitcher.addEventListener("click", function() {
+	if (isCurrentlyLightTheme) {
+		setDarkTheme();
+	} else {
+		setLightTheme();
+	}
+});

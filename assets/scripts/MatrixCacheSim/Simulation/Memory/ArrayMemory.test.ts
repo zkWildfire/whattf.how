ddc3103610e94e2ae092afae6597b4ac8e0fb3fd
Zach Wilson <zach.wilson@wizard.dev>
2023-06-15 23:42:05 +0000

    Add remaining tests for ArrayMemory.

--- assets/scripts/MatrixCacheSim/Simulation/Memory/ArrayMemory.test.ts
+++ assets/scripts/MatrixCacheSim/Simulation/Memory/ArrayMemory.test.ts
@@ -8,3 +8,31 @@ test("Read valid index", () => {
 		expect(memory.read(i)).toBe(VALUES[i]);
 	}
 });
+
+test("Write to memory", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+
+	const INDEX = 2;
+	const VALUE = 42;
+	memory.write(INDEX, VALUE);
+
+	expect(memory.read(INDEX)).toBe(VALUE);
+});
+
+test("Read invalid index", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+
+	const INDEX = 42;
+	expect(() => memory.read(INDEX)).toThrow(RangeError);
+});
+
+test("Write invalid index", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+
+	const INDEX = 42;
+	const VALUE = 42;
+	expect(() => memory.write(INDEX, VALUE)).toThrow(RangeError);
+});

8e03d621b4b42de36193d48685b8c4fa935a0ca7
Zach Wilson <zach.wilson@wizard.dev>
2023-06-15 23:48:38 +0000

    Add WriteThroughCacheLine tests.

---
+++ assets/scripts/MatrixCacheSim/Simulation/CacheLines/WriteThroughCacheLine.test.ts
@@ -0,0 +1,103 @@
+import ArrayMemory from "../Memory/ArrayMemory";
+import WriteThroughCacheLine from "./WriteThroughCacheLine";
+
+test("PropertiesMatchCtorArgs", () => {
+	const INDEX = 1;
+	const SIZE = 3;
+
+	const memory = new ArrayMemory([1, 2, 3, 4, 5]);
+	const cacheLine = new WriteThroughCacheLine(memory, INDEX, SIZE);
+
+	expect(cacheLine.startIndex).toBe(INDEX);
+	expect(cacheLine.endIndex).toBe(INDEX + SIZE);
+	expect(cacheLine.size).toBe(SIZE);
+})
+
+test("Contains", () => {
+	const memory = new ArrayMemory([1, 2, 3, 4, 5]);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	expect(cacheLine.contains(0)).toBe(false);
+	expect(cacheLine.contains(1)).toBe(true);
+	expect(cacheLine.contains(2)).toBe(true);
+	expect(cacheLine.contains(3)).toBe(true);
+	expect(cacheLine.contains(4)).toBe(false);
+});
+
+test("Read value in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 2;
+	expect(cacheLine.read(INDEX)).toBe(VALUES[INDEX]);
+});
+
+test("Read value not in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 0;
+	expect(() => cacheLine.read(INDEX)).toThrow(RangeError);
+});
+
+test("Write value in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 2;
+	const VALUE = 42;
+	cacheLine.write(INDEX, VALUE);
+	cacheLine.flush();
+
+	expect(memory.read(INDEX)).toBe(VALUE);
+});
+
+test("Write value not in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 0;
+	const VALUE = 42;
+	expect(() => cacheLine.write(INDEX, VALUE)).toThrow(RangeError);
+});
+
+test("OnCacheLineAccessed event raised when reading value in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 2;
+
+	let eventRaised = false;
+	cacheLine.OnCacheLineAccessed.subscribe((sender, args) => {
+		expect(sender).toBe(cacheLine);
+		expect(args.index).toBe(INDEX);
+		eventRaised = true;
+	});
+
+	cacheLine.read(INDEX);
+	expect(eventRaised).toBe(true);
+});
+
+test("OnCacheLineAccessed event raised when writing value in cache line", () => {
+	const VALUES = [1, 2, 3, 4, 5];
+	const memory = new ArrayMemory(VALUES);
+	const cacheLine = new WriteThroughCacheLine(memory, 1, 3);
+
+	const INDEX = 2;
+	const VALUE = 42;
+
+	let eventRaised = false;
+	cacheLine.OnCacheLineAccessed.subscribe((sender, args) => {
+		expect(sender).toBe(cacheLine);
+		expect(args.index).toBe(INDEX);
+		eventRaised = true;
+	});
+
+	cacheLine.write(INDEX, VALUE);
+	expect(eventRaised).toBe(true);
+});

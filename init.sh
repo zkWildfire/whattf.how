49570f281a0f35b4e22f48363468019c845e911c
Zach Wilson <zach.wilson@wizard.dev>
2023-06-18 00:17:12 +0000

    Add script code to patch jekyll-webpack.

--- init.sh
+++ init.sh
@@ -7,3 +7,22 @@ set -e
 # Install required gems, packages, etc
 bundle
 npm install
+
+# jekyll-webpack is broken with Ruby >=3.2 (as of 2023-06-17) because it uses
+#   `File.exists` which was deprecated in Ruby 2.1.0 and removed in Ruby 3.2.0.
+# This can be fixed by patching the gem's sources to use `File.exist?` instead.
+JEKYLL_WEBPACK_INSTALL_DIR=$(bundle info jekyll-webpack | grep -oP "(?<=Path: ).*")
+readonly JEKYLL_WEBPACK_INSTALL_DIR
+if [[ -z "$JEKYLL_WEBPACK_INSTALL_DIR" ]]; then
+	echo "Failed to find jekyll-webpack install directory"
+	exit 1
+else
+	echo "Found jekyll-webpack install directory: $JEKYLL_WEBPACK_INSTALL_DIR"
+fi
+
+# Patch all ruby scripts in the jekyll-webpack gem to use `File.exist?` instead
+#   of `File.exists`.
+echo "Patching jekyll-webpack to use File.exist? instead of File.exists..."
+find "$JEKYLL_WEBPACK_INSTALL_DIR" -type f -name "*.rb" \
+	-exec sed -i 's/File.exists?/File.exist?/g' {} \;
+echo "Successfully patched jekyll-webpack."

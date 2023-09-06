912d455b7801e92d907bab31cecedf864fe284ee
Zach Wilson <zach.wilson@wizard.dev>
2023-09-06 02:10:50 +0000

    Implement main menu for the typing game.

--- assets/scripts/Launcher.ts
+++ assets/scripts/Launcher.ts
@@ -1,8 +1,10 @@
 import { runMatrixCacheSim } from "./MatrixCacheSim/MatrixCacheSim";
+import { runTypingGame } from "./TypingGame/TypingGame";
 
 // Mapping of URLs to the functions that should be run
 const urlMap = new Map<string, () => void>([
-	["/posts/matrix-cache-simulator-demo/", runMatrixCacheSim]
+	["/posts/matrix-cache-simulator-demo/", runMatrixCacheSim],
+	["/posts/typing-game/", runTypingGame]
 ]);
 
 // Run the function associated with the current URL
---
+++ assets/scripts/TypingGame/TypingGame.ts
@@ -0,0 +1,3 @@
+export const runTypingGame = () => {
+	console.log('Typing Game');
+}
